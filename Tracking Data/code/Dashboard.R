50a20dd675f03197be0a1c058d56b6bb6042f551
Jeremy Rodriguez <113404239+jrodriguez25@users.noreply.github.com>
2023-10-03 11:53:47 -0400

    update

---
+++ Tracking Data/code/Dashboard.R
@@ -0,0 +1,54 @@
+library(shinydashboard)
+library(shiny)
+library(tidyverse)
+library(lubirdate)
+library(plotly)
+
+
+
+
+
+# Load Data ---------------------------------------------------------------
+
+clean_tracking <- read_csv("Desktop/PMC Github/Tracking Data/data/clean_tracking.csv")
+
+clean_tracking
+
+
+# Wranling continued ------------------------------------------------------
+
+
+
+
+
+
+
+# View App ----------------------------------------------------------------
+
+ui <- dashboardPage(
+  
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody( fluidRow(
+    box(plotOutput("plot1", height = 250)),
+    
+    box(
+      title = "Controls",
+      sliderInput("slider", "Number of observations:", 1, 100, 50)
+    )
+  )
+  )
+)
+
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
+
+shinyApp(ui, server)
+
